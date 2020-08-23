942c8261f27d67e6b5bada0ae370baf99aac339b
thanos <thanos.karousos@gmail.com>
2020-08-23 17:09:52 +0300

    mikrocommit

---
+++ database script.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS users(
+userID VARCHAR(255) NOT NULL PRIMARY KEY,
+userName VARCHAR(50) NOT NULL,
+name VARCHAR(50) NOT NULL,
+surname VARCHAR(50) NOT NULL,
+email TINYTEXT NOT NULL,
+pass LONGTEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS userdata(
+userid VARCHAR(255) NOT NULL REFERENCES users(userID),
+timestampms VARCHAR(14) NOT NULL,
+latitude INT NOT NULL,
+longtitude INT NOT NULL,
+accuracy INT NOT NULL,
+type VARCHAR(50),
+confidence INT(4),
+year INT(10),
+month INT(4),
+day INT(3),
+hour INT(5)
+PRIMARY KEY (userid, timestampms)
+);
+
+CREATE TABLE IF NOT EXISTS activity(
+    userid VARCHAR(255) NOT NULL REFERENCES users(userID),
+    timestampms VARCHAR(14),
+    type ENUM('ON_FOOT', 'WALKING', 'UNKNOWN', 'STILL', 'RUNNING', 'IN_VEHICLE', 'ON_BICYCLE', 'IN_ROAD_VEHICLE', 'IN_RAIL_VEHICLE', 'IN_TWO_WHEELER_VEHICLE', 'IN_FOUR_WHEELER_VEHICLE', 'TILTING'),
+    confidence INT,
+    PRIMARY KEY (userid, timestampms)
+);
