829c8420c5f6e88961a6b10447acc182e213b4d8
thanos <thanos.karousos@gmail.com>
2020-08-23 18:15:44 +0300

    made UI a better place to live in

--- user.php
+++ user.php
@@ -21,7 +21,6 @@ require 'actions/chartaction.php';
 </head>
 <body>
     <div id="mapid"></div>
-	        <!--<p class="fallback">Upload your JSON file: <input name="file" type="file" id="file"></input></p> -->
 
       <form  name="Filter" method="POST">
       From:
@@ -31,23 +30,12 @@ require 'actions/chartaction.php';
       <input type="date" name="dateTo"/>
       <input type="submit" name="submit" value="Dates"/>
 
-
-  	<?php
-  	/*$new_date = strtotime($_POST['dateFrom']);
-  	echo $new_date;
-
-  	echo "<br>";
-  	$new_date2 = strtotime($_POST['dateTo']);
-  	echo $new_date2;
-    */
-  	?>
   </form>
     <script src="leaflet/leaflet.js"></script>
     <script src="heatmap/heatmap.js-master/build/heatmap.js"></script>
     <script src="heatmap/heatmap.js-master/plugins/leaflet-heatmap/leaflet-heatmap.js"></script>
     <script src="leaflet/map.js"> </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
-    <!--<script src="javascript/yearDropDown.js"></script>-->
     <form action="actions/uploadaction.php" method="POST" enctype="multipart/form-data">
         <input type="file" name="jsonfile" id="myFile">
         <button type="submit" name="uploadsubmit">Upload your json file!</button>
